2f4b083edbb5e7cbcb21f534fe4c2dbbfa68449d
mryoyo1555-code <mr.yoyo1555@gmail.com>
2025-07-28 17:09:58 +0300

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,9 @@
 input.onButtonPressed(Button.A, function () {
     TPBot.setTravelSpeed(TPBot.DriveDirection.Forward, 50)
 })
-input.onButtonPressed(Button.B, function () {
-    TPBot.setTravelSpeed(TPBot.DriveDirection.Backward, 50)
-})
 input.onButtonPressed(Button.AB, function () {
     TPBot.stopCar()
+})
+input.onButtonPressed(Button.B, function () {
+    TPBot.setTravelSpeed(TPBot.DriveDirection.Backward, 50)
 })
